c66a27b9bce0deacbedf26aa0e2a4cad2fa33fb1
ndmsnkri <144985362+ndmsnkri@users.noreply.github.com>
2023-09-28 21:21:15 +0300

    Initial commit

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,17 +1,24 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.Scanner;
+
+public class CtoFConverter {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
+        double celsius, fahrenheit;
+        boolean done = false;
+        do {
+            System.out.print("Enter a temperature in Celsius: ");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+            // Check if the input is a valid double
+            if (in.hasNextDouble()) {
+                celsius = in.nextDouble();
+                fahrenheit = (celsius * 9/5) + 32;
+                System.out.println(celsius + " C" + " in Fahrenheit is : " + fahrenheit + " F");
+                done = true;
+            } else {
+                System.out.println("Invalid input. Please enter a valid temperature in Celsius.");
+                in.nextLine(); // Clear the invalid input
+            }
+        } while (!done);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
